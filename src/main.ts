5864e1c797b0302289db6dea562c357e4154cce7
Sinclair <truly.sinclair@gmail.com>
2024-01-30 18:08:24 -0500

    feat: basic example

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserView, BrowserWindow, Rectangle } from 'electron';
 import path from 'path';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -8,19 +8,64 @@ if (require('electron-squirrel-startup')) {
 
 const createWindow = () => {
   // Create the browser window.
+  let mainWindowBounds = { width: 800, height: 600 };
+  let mainViewBounds = { x: 248, y: 20, width: mainWindowBounds.width - 248, height: 600 };
+
+  let childWindowBounds = { height: 600, width: 800 };
+  let childWindowRects: Rectangle[] = [
+    { x: 0, y: 0, width: childWindowBounds.width, height: 20 },
+    { x: 0, y: 20, width: 48, height: childWindowBounds.height - 20 },
+    { x: 48, y: 20, width: 200, height: childWindowBounds.height - 20 },
+    { x: 0, y: childWindowBounds.height - 20, width: childWindowBounds.width, height: 20 },
+  ]
+
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: mainWindowBounds.width,
+    height: mainWindowBounds.height,
+    frame: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
+  const mainView = new BrowserView()
+
+  mainWindow.setBrowserView(mainView)
+  mainView.setBounds(mainViewBounds)
+  mainView.webContents.loadURL('https://www.youtube.com')
+
+  const childWindow = new BrowserWindow({
+    width: childWindowBounds.width,
+    height: childWindowBounds.height,
+    parent: mainWindow,
+    frame: false,
+    transparent: true,
+  });
+
+  childWindow.setShape(childWindowRects)
+
+
+  childWindow.on('resize', () => {
+    mainWindow.setContentBounds(childWindow.getContentBounds())
+
+    const { x, y, width, height } = childWindow.getBounds()
+
+    mainView.setBounds({
+      x: 0, y: 0,
+      width: width,
+      height: height
+    })
+  })
+
+  childWindow.on('move', () => {
+    mainWindow.setContentBounds(childWindow.getContentBounds())
+  })
+
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    childWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    childWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
 
   // Open the DevTools.
